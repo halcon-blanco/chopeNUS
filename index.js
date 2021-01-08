dc14567ee68bcc8e1ae66d1d733e1fff87cfc260
halcon-blanco <saadh98@gmail.com>
2021-01-08 13:16:06 +0800

    add test buttons

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const Telegraf = require('telegraf')
+const {
+    Telegraf,
+    Markup
+} = require('telegraf')
 
 const bot = new Telegraf('1547617487:AAFd7SY_CnefKeC5DJ_sqI9LX0xCUNvt0O8')
 
 
 bot.start(ctx => {
     ctx.reply('Welcome to ChopeNUS! Which type of facility do you want to book?');
-})
+
+    const buttons = Markup.inlineKeyboard([
+        [Markup.callbackButton('Test', 'test')],
+        [Markup.callbackButton('Test 2', 'test2')]
+    ]);
+
+    ctx.telegram.sendMessage(ctx.chat.id, "test", {
+        reply_markup: buttons
+    })
+
+
+});
 
 bot.launch()
