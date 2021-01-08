a5639d4ae139e82d6eb640a123adf0e6f06b42dd
umergta <umergta@gmail.com>
2021-01-09 07:20:40 +0800

    debug

--- database.js
+++ database.js
@@ -2,17 +2,24 @@ const {Pool} = require('pg');
 const chopenus = require('pg').Pool;
 require("dotenv").config();
 
-const devConfig = {
-   user : process.env.PG_USER,
-   host: process.env.PG_HOST,
-   database: process.env.PG_DATABASE,
-   port:process.env.PORT,
-   ssl : true
-};
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl:{
+        rejectUnauthorized: false
+    }
+});
+pool.connect();
+// const devConfig = {
+//    user : process.env.PG_USER,
+//    host: process.env.PG_HOST,
+//    database: process.env.PG_DATABASE,
+//    port:process.env.PORT,
+//    ssl : true
+// };
 
-const proConfing = {
-    connectionString: process.env.DATABASE_URL
-}
-const pool = new Pool(process.env.NODE_ENV === 'production' ? proConfing : devConfig);
+// const proConfing = {
+//     connectionString: process.env.DATABASE_URL
+// }
+// const pool = new Pool(process.env.NODE_ENV === 'production' ? proConfing : devConfig);
 
 module.exports = pool;
