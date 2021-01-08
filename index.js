ffc0ef885f87468c0bfbbf00cce7de08e4a26251
umergta <umergta@gmail.com>
2021-01-08 14:37:13 +0800

    added correct link for web scraping

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const {
     Telegraf,
     Markup
 } = require('telegraf')
+
+//web links
+
 const SPORTS = "https://reboks.nus.edu.sg/nus_public_web/public/"
 const UTOWN = "https://uci.nus.edu.sg/suu/facilities-booking/facilities-booking-utown-non-residential-facilities/"
 const LIBRARIES = "https://libportal.nus.edu.sg/frontend/web/bookdiscussionroom"
 const WEB_VPN = "https://webvpn.nus.edu.sg/dana-na/auth/url_default/welcome.cgi"
-// sampler for
-const COM_SCHEDULE = "https://mysoc.nus.edu.sg/~calendar/getBooking.cgi?room=MR1"
+// can use for web-scraping for schedule later as an e.g. (computing facilities)
+const COM_SCHEDULE = "https://mysoc.nus.edu.sg/~calendar/getBooking.cgi?"
+
 const bot = new Telegraf('1547617487:AAFd7SY_CnefKeC5DJ_sqI9LX0xCUNvt0O8')
 
 
