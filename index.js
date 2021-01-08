2658dd07a5ced03f6e224d7ff2ee70376daac4e4
halcon-blanco <saadh98@gmail.com>
2021-01-08 14:31:28 +0800

    restructure buttons

--- index.js
+++ index.js
@@ -7,23 +7,15 @@ const bot = new Telegraf('1547617487:AAFd7SY_CnefKeC5DJ_sqI9LX0xCUNvt0O8')
 
 
 bot.start(ctx => {
-    ctx.reply('Welcome to ChopeNUS! Which type of facility do you want to book?');
+    ctx.reply('');
 
     const buttons = Markup.inlineKeyboard([
-        [Markup.callbackButton('FOE', 'foe')],
-        [Markup.callbackButton('Music', 'music')],
-        [Markup.callbackButton('Medicine', 'med')],
-        [Markup.callbackButton('YIH', 'yih')],
-        [Markup.callbackButton('UTown', 'utown')],
-        [Markup.callbackButton('FOE', 'foe')],
-        [Markup.callbackButton('FASS', 'fass')],
-        [Markup.callbackButton('SOC', 'soc')],
-        [Markup.callbackButton('FOS', 'fos')],
-        [Markup.callbackButton('SDE', 'sde')],
-        [Markup.callbackButton('Law', 'law')],
+        [Markup.callbackButton('FOE', 'foe'), Markup.callbackButton('Music', 'music'), Markup.callbackButton('Medicine', 'med'), Markup.callbackButton('YIH', 'yih')],
+        [Markup.callbackButton('UTown', 'utown'), Markup.callbackButton('FOE', 'foe'), Markup.callbackButton('FASS', 'fass'), Markup.callbackButton('SOC', 'soc')],
+        [Markup.callbackButton('FOS', 'fos'), Markup.callbackButton('SDE', 'sde'), Markup.callbackButton('Law', 'law')]
     ]);
 
-    ctx.telegram.sendMessage(ctx.chat.id, "test", {
+    ctx.telegram.sendMessage(ctx.chat.id, "Welcome to ChopeNUS! Which type of facility do you want to book?", {
         reply_markup: buttons
     })
 
